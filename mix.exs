461a8f634ddd422320939b14e44d3ea1d78b34bf
Pedro <palmelapedro@gmail.com>
2023-05-29 11:33:22 +0200

    added hackney

--- mix.exs
+++ mix.exs
@@ -51,8 +51,9 @@ defmodule Bmvp.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:hackney, "~> 1.18"},
 
-      #dev
+      # dev
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
